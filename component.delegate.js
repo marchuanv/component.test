0f094ba0ca56f429dac6ea7a7ed048f51b46e340
marchuanv <marchuanv@gmail.com>
2020-06-09 01:13:39 +0200

    refactor

--- component.delegate.js
+++ component.delegate.js
@@ -3,16 +3,33 @@ logging.config.add("Delegating");
 module.exports = { 
     retry: 0,
     pointers: [],
-    create: (moduleName, callback, callbackRetry) => {
-        pointers.push({ moduleName, callback, callbackRetry });
+    register: ( moduleName, callingModuleName, callback ) => {
+        const pointer = module.exports.pointers.find(mod => mod.name === moduleName && mod.dependancy === callingModuleName);
+        if (pointer){
+            pointer.call = callback;
+        } else {
+            module.exports.pointers.push({ 
+                name: moduleName, 
+                dependancy: callingModuleName,
+                call: callback
+            });
+        }
     },
-    call: async (callingFunc, rollbackFunc, params) => {
+    registerRetry: ( moduleName, callingModuleName, callback ) => {
+        const pointer = module.exports.pointers.find(mod => mod.name === moduleName && mod.dependancy === callingModuleName);
+        if (pointer){
+            pointer.retry = callback;
+        } else {
+            module.exports.pointers.push({ 
+                name: moduleName, 
+                dependancy: callingModuleName,
+                retry: callback
+            });
+        }
+    },
+    call: async (callingFunc, params) => {
         if (!callingFunc || typeof callingFunc !== 'function'){
             logging.write("Delegating", `expected parameter 'callingFunc' to be a function`);
-            return;
-        }
-        if (rollbackFunc && typeof rollbackFunc !== 'function'){
-            logging.write("Delegating", `expected parameter 'rollbackFunc' to be a function`);
             return;
         }
         let error;
@@ -24,14 +41,7 @@ module.exports = {
         if (error && module.exports.retry <= 2 ){
             module.exports.retry = module.exports.retry + 1;
             logging.write("Delegating", `${callingFunc.name} error after ${module.exports.retry} retries.`);
-            logging.write("Delegating", `calling ${rollbackFunc.name}.`);
-            try {
-                await rollbackFunc(params);
-            } catch (err) {
-                logging.write("Delegating", `${rollbackFunc.name} failed with: ${err.message}.`);
-                return;
-            }
-            return module.exports.call(callingFunc, rollbackFunc, params);
+            return module.exports.call(callingFunc, params);
         } else if (error) {
             logging.write("Delegating", `${callingFunc.name} failed with: ${error.message}.`);
             throw error;

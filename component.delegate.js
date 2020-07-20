a4b2e2c33e0b64f6c7b91dac587a1ac7d9e67c44
marchuanv <marchuanv@gmail.com>
2020-07-20 23:55:21 +0200

    refactor

--- component.delegate.js
+++ component.delegate.js
@@ -32,17 +32,17 @@ module.exports = {
             let error;
             try {
                 const result = await callback.func(params);
-                results.push( { name: callback.name, result });
+                results.push( { name: callback.name, result, isError: false });
             } catch (err) {
                 error = err;
             }
             if (error && module.exports.retry <= 2 ){
                 module.exports.retry = module.exports.retry + 1;
                 logging.write("Delegating", `${callback.name} error after ${module.exports.retry} retries.`);
-                results = results.concat(module.exports.call( { context, name: callback.name }, params));
+                results = results.concat(await module.exports.call( { context, name: callback.name }, params));
             } else if (error) {
                 logging.write("Delegating", `${callback.name} failed with: ${error.message || error}.`);
-                results.push( { name: callback.name, error });
+                results.push( { name: callback.name, result: error, isError: true });
             }
         }
         return results;
--- test.js
+++ test.js
@@ -1,13 +1,18 @@
 const componentDelegate = require("./component.delegate.js");
+
 const callingModule = "something";
-componentDelegate.register(callingModule, "RandomFunction", () => {
+componentDelegate.register(callingModule, "RandomFunction1", () => {
     throw new Error("some random error occured");
 });
+componentDelegate.register(callingModule, "RandomFunction2", () => {
+    return "Success";
+});
+
 
 (async() => {
-    const prom = new Promise((resolve) => {
+    const prom = new Promise(async (resolve) => {
         const parameters = {};
-        componentDelegate.call( { context: callingModule }, parameters);
+        const results = await componentDelegate.call( { context: callingModule }, parameters);
         setTimeout(resolve,1000);
     });
     await prom;

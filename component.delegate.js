a9ba7c1e8fadec231308796b7b29466f551431a9
marchuanv <marchuanv@gmail.com>
2021-02-03 01:18:08 +0200

    fix

--- component.delegate.js
+++ component.delegate.js
@@ -43,7 +43,7 @@ module.exports = {
             return  new Error(error);
         }
 
-        const filteredCallbacks = callbacks.filter(c => c.name.startsWith(wildcard) || ( (wildcard === undefined || wildcard === "") && (c.name === name || !name )) );
+        const filteredCallbacks = callbacks.filter(c => c.name.toString().startsWith(wildcard) || ( (wildcard === undefined || wildcard === "") && (c.name === name || !name )) );
         if (filteredCallbacks.length === 0){
             const error = `no callbacks`;
             logging.write("Delegating", error);

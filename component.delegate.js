1045f6ae3ca5e1dec1751081abbeddc28c24603a
marchuanv <marchuanv@gmail.com>
2021-03-05 13:14:53 +0200

    refactor

--- component.delegate.js
+++ component.delegate.js
@@ -4,33 +4,126 @@ const fs = require("fs");
 const callstackFile = `${__dirname}/callstack.json`;
 let stack = [];
 
-const terminate = () => {
+const saveCallstack = () => {
     console.log('saving callstack.');
     fs.writeFileSync(callstackFile,JSON.stringify(stack,null,4));
 };
 
-process.on('SIGTERM', () => terminate() );
-process.on('exit', () => terminate() );
-process.on('SIGINT', () => terminate() );
-process.on('SIGUSR1', () => terminate() );
-process.on('SIGUSR2', () => terminate() );
-process.on('uncaughtException', () => terminate() );
+process.on('SIGTERM', () => saveCallstack() );
+process.on('exit', () => saveCallstack() );
+process.on('SIGINT', () => saveCallstack() );
+process.on('SIGUSR1', () => saveCallstack() );
+process.on('SIGUSR2', () => saveCallstack() );
+process.on('uncaughtException', () => saveCallstack() );
 
 const locks = [];
+module.exports = {
+    pointers: [],
+    call: async ( { context, name, wildcard }, params) => {
+        
+        const contextLockName = context || "global";
+        let contextLock = locks.find(x => x.context === contextLockName);
+        if (!contextLock) {
+            contextLock = { isLocked: true, context: contextLockName };
+            locks.push(contextLock);
+        } else if (!contextLock.isLocked) {
+            contextLock.isLocked = true;
+        } else {
+            return new Promise((resolve)=> {
+                setTimeout(async () => {
+                    const results = await module.exports.call( { context, name, wildcard }, params);
+                    resolve(results);
+                }, 1000);
+            });
+        }
 
-component.require("component.logging", { gitUsername: "marchuanv" });
+        if (!context){
+            const error = "failed to invoke callback, no context provided.";
+            componentLogging.write("Delegating", error);
+            contextLock.isLocked = false
+            return new Error(error);
+        }
+        
+        const pointer = module.exports.pointers.find(p => p.context === context);
+        if (!pointer){
+            const error = `no pointers found for the ${context} module.`;
+            componentLogging.write("Delegating", error);
+            contextLock.isLocked = false
+            return new Error(error);
+        }
 
-component.events.on({ moduleName: "component.logging", eventType: "register" },({ componentLogging }) => {
-    
-    component.events.on({ eventType: "register" }, ({ module }) => {
-        const name = `${module.port}${module.path}`;
-        const event = component.events.find({ moduleName: module.name, eventType: "module" });
-        const context = module.name;
-        const overwriteDelegate = module.overwriteDelegate;
+        const callbacks =  pointer.callbacks;
+        if (!callbacks || !Array.isArray(callbacks)){
+            const error = `expected pointer 'callbacks' to be an array`;
+            componentLogging.write("Delegating",error);
+            contextLock.isLocked = false
+            return  new Error(error);
+        }
+
+        const filteredCallbacks = callbacks.filter(c => c.name.toString().startsWith(wildcard) || ( (wildcard === undefined || wildcard === "") && (c.name === name || !name )) );
+        if (filteredCallbacks.length === 0){
+            const error = `no callbacks`;
+            componentLogging.write("Delegating", error);
+            contextLock.isLocked = false
+            return new Error(error);
+        }
+        
+        for(const callback of filteredCallbacks){
+            try {
+                componentLogging.write("Delegating", "invoking callback");
+                const stackItem = { context, name: callback.name, retry: callback.retry, date: new Date() };
+                stack.push(stackItem);
+                callback.result = await callback.func(params);
+                componentLogging.write("Delegating", "callback invoked");
+                callback.timeout = 500;
+                callback.retry = 1;
+            } catch (error) {
+                componentLogging.write("Delegating", `${callback.name} failed with: ${error.message || error}, retrying ${callback.retry} of 3`);
+                callback.result = error;
+                if (callback.retry <= 2){
+                    callback.retry = callback.retry + 1;
+                    setTimeout(async () => {
+                        await module.exports.call( { context, name: callback.name, wildcard }, params);
+                    }, callback.timeout);
+                }
+                callback.timeout = callback.timeout * 2;
+            }
+        };
+
+        //Errors before promises resolved
+        for(const errorResult of filteredCallbacks.filter(cb => cb.result && cb.result.message && cb.result.stack)){
+            contextLock.isLocked = false
+            return errorResult.result;
+        };
+
+        await Promise.all(filteredCallbacks.map(c => c.result));
+        
+        componentLogging.write("Delegating", "callback(s) invoked");
+
+        const filteredCallbacksCloned = JSON.parse(JSON.stringify(filteredCallbacks));
+        filteredCallbacks.forEach(x => x.result = null );
+
+        //Errors after promises resolved
+        for(const errorResult of filteredCallbacksCloned.filter(cb => cb.result && cb.result.message && cb.result.stack)){
+            contextLock.isLocked = false
+            return errorResult.result;
+        };
+
+        if (filteredCallbacksCloned.filter(cb => cb.result).length > 1){
+            contextLock.isLocked = false
+            return new Error(`expected at most one of all the functions registered for "${context}" to return results`);
+        }
+
+        contextLock.isLocked = false
+
+        const firstCallbackWithResult = filteredCallbacksCloned.find(cb => cb.result);
+        return  firstCallbackWithResult? firstCallbackWithResult.result : null;
+    },
+    register: async ({ context, name, callback, overwriteDelegate = true}) => {
+        const event = component.events.find({ moduleName: name, eventType: "module" });
         const pointer = module.exports.pointers.find(p => p.context === context);
         if (pointer){
             if (overwriteDelegate){
-                componentLogging.write("Delegating", `Overwriting existing ${name} callback on ${context}`);
                 const duplicateCallbackIndex = pointer.callbacks.findIndex(x => x.name === name);
                 if (duplicateCallbackIndex > -1){
                     pointer.callbacks.splice(duplicateCallbackIndex,1);
@@ -44,109 +137,5 @@ component.events.on({ moduleName: "component.logging", eventType: "register" },(
             });
             componentLogging.write("Delegating", `Registered ${name} callback on ${context}`);
         }
-    });
-    
-    module.exports = {
-        pointers: [],
-        call: async ( { context, name, wildcard }, params) => {
-            
-            const contextLockName = context || "global";
-            let contextLock = locks.find(x => x.context === contextLockName);
-            if (!contextLock) {
-                contextLock = { isLocked: true, context: contextLockName };
-                locks.push(contextLock);
-            } else if (!contextLock.isLocked) {
-                contextLock.isLocked = true;
-            } else {
-                return new Promise((resolve)=> {
-                    setTimeout(async () => {
-                        const results = await module.exports.call( { context, name, wildcard }, params);
-                        resolve(results);
-                    }, 1000);
-                });
-            }
-    
-            if (!context){
-                const error = "failed to invoke callback, no context provided.";
-                componentLogging.write("Delegating", error);
-                contextLock.isLocked = false
-                return new Error(error);
-            }
-            
-            const pointer = module.exports.pointers.find(p => p.context === context);
-            if (!pointer){
-                const error = `no pointers found for the ${context} module.`;
-                componentLogging.write("Delegating", error);
-                contextLock.isLocked = false
-                return new Error(error);
-            }
-    
-            const callbacks =  pointer.callbacks;
-            if (!callbacks || !Array.isArray(callbacks)){
-                const error = `expected pointer 'callbacks' to be an array`;
-                componentLogging.write("Delegating",error);
-                contextLock.isLocked = false
-                return  new Error(error);
-            }
-    
-            const filteredCallbacks = callbacks.filter(c => c.name.toString().startsWith(wildcard) || ( (wildcard === undefined || wildcard === "") && (c.name === name || !name )) );
-            if (filteredCallbacks.length === 0){
-                const error = `no callbacks`;
-                componentLogging.write("Delegating", error);
-                contextLock.isLocked = false
-                return new Error(error);
-            }
-            
-            for(const callback of filteredCallbacks){
-                try {
-                    componentLogging.write("Delegating", "invoking callback");
-                    const stackItem = { context, name: callback.name, retry: callback.retry, date: new Date() };
-                    stack.push(stackItem);
-                    callback.result = await callback.func(params);
-                    componentLogging.write("Delegating", "callback invoked");
-                    callback.timeout = 500;
-                    callback.retry = 1;
-                } catch (error) {
-                    componentLogging.write("Delegating", `${callback.name} failed with: ${error.message || error}, retrying ${callback.retry} of 3`);
-                    callback.result = error;
-                    if (callback.retry <= 2){
-                        callback.retry = callback.retry + 1;
-                        setTimeout(async () => {
-                            await module.exports.call( { context, name: callback.name, wildcard }, params);
-                        }, callback.timeout);
-                    }
-                    callback.timeout = callback.timeout * 2;
-                }
-            };
-    
-            //Errors before promises resolved
-            for(const errorResult of filteredCallbacks.filter(cb => cb.result && cb.result.message && cb.result.stack)){
-                contextLock.isLocked = false
-                return errorResult.result;
-            };
-    
-            await Promise.all(filteredCallbacks.map(c => c.result));
-            
-            componentLogging.write("Delegating", "callback(s) invoked");
-    
-            const filteredCallbacksCloned = JSON.parse(JSON.stringify(filteredCallbacks));
-            filteredCallbacks.forEach(x => x.result = null );
-    
-            //Errors after promises resolved
-            for(const errorResult of filteredCallbacksCloned.filter(cb => cb.result && cb.result.message && cb.result.stack)){
-                contextLock.isLocked = false
-                return errorResult.result;
-            };
-    
-            if (filteredCallbacksCloned.filter(cb => cb.result).length > 1){
-                contextLock.isLocked = false
-                return new Error(`expected at most one of all the functions registered for "${context}" to return results`);
-            }
-    
-            contextLock.isLocked = false
-    
-            const firstCallbackWithResult = filteredCallbacksCloned.find(cb => cb.result);
-            return  firstCallbackWithResult? firstCallbackWithResult.result : null;
-        }
-    };
-});
+    }
+};

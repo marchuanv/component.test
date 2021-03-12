ec5c8b20ead86d85cef439e5ac9a6593c8bf86c5
marchuanv <marchuanv@gmail.com>
2021-03-12 02:46:34 +0200

    refactor

--- test.js
+++ test.js
@@ -1,1 +1,6 @@
-require("component.request.handler");
+(async()=> {
+    const component = require("component");
+    await component.load({ moduleName:"component.request.handler", gitUsername: "marchuanv" });
+    console.log("test");
+    //require("component.request.handler");
+})();

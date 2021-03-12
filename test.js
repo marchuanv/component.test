6dadcb73d27b8cf27768be339319e154a3a32b83
marchuanv <marchuanv@gmail.com>
2021-03-12 04:49:21 +0200

    refactor

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 (async()=> {
     const component = require("component");
-    await component.load({ moduleName:"component.request.handler", gitUsername: "marchuanv" });
+    await component.load({ moduleName:"component.request.handler.route" });
     console.log("test");
     //require("component.request.handler");
 })();

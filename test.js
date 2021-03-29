e868124fbf5833df8dbd320b845328ead338fa51
marchuanv <marchuanv@gmail.com>
2021-03-30 00:22:37 +0200

    refactor

--- test.js
+++ test.js
@@ -3,3 +3,4 @@ require("./component.request.handler.deferred.test.js");
 require("./component.request.handler.user.test.js");
 require("./component.request.handler.route.test.js");
 require("./component.request.handler.unsecure.test.js");
+require("./component.request.handler.secure.test.js");

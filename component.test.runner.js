3f3f1865031b2121c6ef8d47710fa30ecda48f35
marchuanv <marchuanv@gmail.com>
2021-04-15 02:42:58 +0200

    refactor

---
+++ component.test.runner.js
@@ -0,0 +1,63 @@
+const { bootstrap } = require("./bootstrap.js");
+const utils = require("utils");
+module.exports = {
+    runTest: ( { componentName, requestPath, username, passphrase, inputData = "", isNewRouteroute = false, statusCode = 200, statusMessage = "Success" }) => {
+        return new Promise(async (resolve, reject) => {
+            const { request, component, complete } = await bootstrap(componentName);
+            let newRequest = {
+                port: 3000,
+                path: requestPath,
+                method: "GET",
+                headers: {},
+                data: inputData
+            };
+            if (username){
+                newRequest.headers.username = username;
+                newRequest.headers.fromport = 4000;
+                newRequest.headers.fromhost = "localhost";
+            }
+            if (isNewRouteroute) {
+                newRequest.path = "/routes/register";
+                if (passphrase) {
+                    const { hashedPassphrase, hashedPassphraseSalt } = utils.hashPassphrase(passphrase);
+                    newRequest.data = utils.getJSONString({ path: requestPath, hashedPassphrase, hashedPassphraseSalt });
+                } else {
+                    newRequest.data = utils.getJSONString({ path: requestPath });
+                }
+                const results = await request.send(newRequest);
+                if (results.statusCode !== 200){
+                    reject(results);
+                    return complete();
+                }
+                newRequest.path = requestPath;
+                newRequest.data = inputData;
+            }
+            if (passphrase) {
+                newRequest.headers.passphrase = passphrase;
+            }
+            component.receiveDependantComponentNotifications(() => {
+                return {
+                    statusCode: 200,
+                    statusMessage: "Success",
+                    headers: {}
+                };
+            });
+            const results = await request.send(newRequest);
+            if (results.statusCode === statusCode && results.statusMessage === statusMessage){
+                await resolve( utils.getJSONString({ 
+                    status: "Passed",
+                    componentName, 
+                    response: results
+                }));
+            } else {
+                await reject( utils.getJSONString({ 
+                    status: "Failed",
+                    componentName, 
+                    response: results
+                }));
+            }
+            complete();
+        });
+    }
+}
+
--- test.js
+++ test.js
@@ -1,6 +1,37 @@
-require("./component.request.handler.test.js");
-require("./component.request.handler.deferred.test.js");
-require("./component.request.handler.user.test.js");
-require("./component.request.handler.route.test.js");
-require("./component.request.handler.secure.test..js");
+const testRunner = require("./component.test.runner");
+
+( async () => {
+    await testRunner.runTest({ componentName: "component.request.handler", requestPath: "/request"});
+    await testRunner.runTest({ componentName: "component.request.handler.deferred", requestPath: "/deferred"});
+    
+    //Test with user headers
+    await testRunner.runTest({ componentName: "component.request.handler.user", requestPath: "/user", username: "joe", statusCode: 200, statusMessage: "Success" });
+    //Test without user headers
+    await testRunner.runTest({ componentName: "component.request.handler.user", requestPath: "/user", statusCode: 400, statusMessage: "Bad Request" });
+    
+    // //No Routes
+    // await testRunner.runTest({ componentName: "component.request.handler.route", username: "joe", requestPath: "/test", statusCode: 404, statusMessage: "Not Found" });
+    // //Register Test Route
+    // await testRunner.runTest({ componentName: "component.request.handler.route", username: "joe", requestPath: "/test", isNewRouteroute: true });
+
+    // //unsecure
+    // await testRunner.runTest({ componentName: "component.request.handler.secure", username: "joe", requestPath: "/unsecure", isNewRouteroute: true });
+
+    // //secure
+    // await testRunner.runTest({ componentName: "component.request.handler.secure", username: "joe", passphrase: "secret1", requestPath: "/secure", isNewRouteroute: true });
+
+    console.log("All Tests Passed");
+})().catch((err) => {
+    console.log(err);
+});
+
+
+
+// testRunner.runTest({ componentName: "component.request.handler.secure", requestPath: "/securerequest", username: "joe", passphrase: "secret1", inputData: "", isNewRouteroute: true });
+
+// require("./component.request.handler.test.js");
+// require("./component.request.handler.deferred.test.js");
+// require("./component.request.handler.user.test.js");
+// require("./component.request.handler.route.test.js");
+// require("./component.request.handler.secure.test..js");
 // require("./component.request.handler.unsecure.test.js");
